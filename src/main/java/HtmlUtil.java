cbb3d88fb4234302f77fa8c1c359ac106c65b63d
Stefan Heidekrüger <9562632+heidekrueger@users.noreply.github.com>
2021-06-07 12:52:38 +0200

    refactor htmlutil

--- src/main/java/HtmlUtil.java
+++ src/main/java/HtmlUtil.java
@@ -4,47 +4,68 @@ import fitnesse.wiki.*;
 
 public class HtmlUtil {
 
+    /**
+     * This function parses the HTML page and searches for "tests" that could be executed.
+     * If no tests are found, the HTML content is returned as is.
+     * @param pageData
+     * @param includeSuiteSetup
+     * @return
+     * @throws Exception
+     */
     public static String testableHtml(PageData pageData, boolean includeSuiteSetup) throws Exception {
-        WikiPage wikiPage = pageData.getWikiPage();
-        StringBuffer buffer = new StringBuffer();
 
         if (pageData.hasAttribute("Test")) {
-            if (includeSuiteSetup) {
-                WikiPage suiteSetup = PageCrawlerImpl.getInheritedPage(SuiteResponder.SUITE_SETUP_NAME, wikiPage);
-                if (suiteSetup != null) {
-                    WikiPagePath pagePath = wikiPage.getPageCrawler().getFullPath(suiteSetup);
-                    String pagePathName = PathParser.render(pagePath);
-                    buffer.append("!include -setup .").append(pagePathName).append("\n");
-                }
-            }
-            WikiPage setup = PageCrawlerImpl.getInheritedPage("SetUp", wikiPage);
-            if (setup != null) {
-                WikiPagePath setupPath = wikiPage.getPageCrawler().getFullPath(setup);
-                String setupPathName = PathParser.render(setupPath);
-                buffer.append("!include -setup .").append(setupPathName).append("\n");
-            }
+            String wrappedContent = wrapTestableHtmlContent(pageData, includeSuiteSetup);
+            pageData.setContent(wrappedContent);
         }
-
-        buffer.append(pageData.getContent());
-        if (pageData.hasAttribute("Test")) {
-            WikiPage teardown = PageCrawlerImpl.getInheritedPage("TearDown", wikiPage);
-            if (teardown != null) {
-                WikiPagePath tearDownPath = wikiPage.getPageCrawler().getFullPath(teardown);
-                String tearDownPathName = PathParser.render(tearDownPath);
-                buffer.append("!include -teardown .").append(tearDownPathName).append("\n");
-            }
-            if (includeSuiteSetup) {
-                WikiPage suiteTeardown = PageCrawlerImpl.getInheritedPage(SuiteResponder.SUITE_TEARDOWN_NAME, wikiPage);
-                if (suiteTeardown != null) {
-                    WikiPagePath pagePath = wikiPage.getPageCrawler().getFullPath(suiteTeardown);
-                    String pagePathName = PathParser.render(pagePath);
-                    buffer.append("!include -teardown .").append(pagePathName).append("\n");
-                }
-            }
-        }
-
-        pageData.setContent(buffer.toString());
         return pageData.getHtml();
     }
 
+    /**
+     * This function wraps the content of a page that contains tests into the appropriate setup and teardown blocks.
+     * @param pageData
+     * @param includeSuiteSetup
+     * @return
+     * @throws Exception
+     */
+    private static String wrapTestableHtmlContent(PageData pageData, boolean includeSuiteSetup) throws Exception {
+
+        WikiPage wikiPage = pageData.getWikiPage();
+
+        StringBuffer buffer = new StringBuffer();
+        appendSetupContent(buffer, wikiPage, includeSuiteSetup);
+        buffer.append(pageData.getContent());
+        appendTeardownContent(buffer, wikiPage, includeSuiteSetup);
+
+        return buffer.toString();
+    }
+
+    private static void appendSetupContent(StringBuffer buffer, WikiPage wikiPage, boolean includeSuiteSetup)
+            throws Exception {
+        String contentFlag = "setup";
+        if (includeSuiteSetup) {
+            appendContentBlock(buffer, wikiPage, SuiteResponder.SUITE_SETUP_NAME, contentFlag);
+        }
+        appendContentBlock(buffer, wikiPage,"SetUp", contentFlag);
+    }
+
+    private static void appendTeardownContent(StringBuffer buffer, WikiPage wikiPage, boolean includeSuiteSetup)
+            throws Exception{
+        String contentFlag = "teardown";
+        appendContentBlock(buffer, wikiPage, "TearDown", contentFlag);
+
+        if (includeSuiteSetup) {
+            appendContentBlock(buffer, wikiPage, SuiteResponder.SUITE_TEARDOWN_NAME, contentFlag);
+        }
+    }
+
+    private static void appendContentBlock(StringBuffer buffer, WikiPage wikiPage, String pageName, String contentFlag)
+            throws Exception {
+        WikiPage page = PageCrawlerImpl.getInheritedPage(pageName, wikiPage);
+        if (page != null) {
+            WikiPagePath pagePath = wikiPage.getPageCrawler().getFullPath(page);
+            String pagePathName = PathParser.render(pagePath);
+            buffer.append("!include -").append(contentFlag).append(" .").append(pagePathName).append("\n");
+        }
+    }
 }
