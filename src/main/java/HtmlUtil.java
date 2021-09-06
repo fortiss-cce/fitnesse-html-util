ecd20ee1d2aa5b8fae501ae0ffd49a6f73824728
Jonas Jelten <jj@sft.lol>
2021-09-06 12:21:51 +0200

    code beautifications

--- src/main/java/HtmlUtil.java
+++ src/main/java/HtmlUtil.java
@@ -1,45 +1,42 @@
 import fitnesse.responders.run.SuiteResponder;
-import fitnesse.wiki.*;
+import fitnesse.wiki.PageCrawler;
+import fitnesse.wiki.PageCrawlerImpl;
+import fitnesse.wiki.PageData;
+import fitnesse.wiki.PathParser;
+import fitnesse.wiki.WikiPage;
+import fitnesse.wiki.WikiPagePath;
 
 
 public class HtmlUtil {
 
+    /**
+     * Generate a wiki page html and wrapper its content with the test suite.
+     * If `includeSuiteSetup` is true, wrapper it again with the test suite setup includes.
+     *
+     * @param pageData: the wiki page to generate and anotate
+     * @param includeSuiteSetup: if test suite setup includes should be added
+     */
     public static String testableHtml(PageData pageData, boolean includeSuiteSetup) throws Exception {
         WikiPage wikiPage = pageData.getWikiPage();
-        StringBuffer buffer = new StringBuffer();
+        StringBuilder buffer = new StringBuilder();
 
         if (pageData.hasAttribute("Test")) {
             if (includeSuiteSetup) {
-                WikiPage suiteSetup = PageCrawlerImpl.getInheritedPage(SuiteResponder.SUITE_SETUP_NAME, wikiPage);
-                if (suiteSetup != null) {
-                    WikiPagePath pagePath = wikiPage.getPageCrawler().getFullPath(suiteSetup);
-                    String pagePathName = PathParser.render(pagePath);
-                    buffer.append("!include -setup .").append(pagePathName).append("\n");
-                }
+                HtmlUtil.suiteInclude(buffer, SuiteResponder.SUITE_SETUP_NAME,
+                                      wikiPage, SuiteResponder.SUITE_SETUP_NAME);
             }
-            WikiPage setup = PageCrawlerImpl.getInheritedPage("SetUp", wikiPage);
-            if (setup != null) {
-                WikiPagePath setupPath = wikiPage.getPageCrawler().getFullPath(setup);
-                String setupPathName = PathParser.render(setupPath);
-                buffer.append("!include -setup .").append(setupPathName).append("\n");
-            }
+
+            HtmlUtil.suiteInclude(buffer, "SetUp", wikiPage, SuiteResponder.SUITE_SETUP_NAME);
         }
 
         buffer.append(pageData.getContent());
+
         if (pageData.hasAttribute("Test")) {
-            WikiPage teardown = PageCrawlerImpl.getInheritedPage("TearDown", wikiPage);
-            if (teardown != null) {
-                WikiPagePath tearDownPath = wikiPage.getPageCrawler().getFullPath(teardown);
-                String tearDownPathName = PathParser.render(tearDownPath);
-                buffer.append("!include -teardown .").append(tearDownPathName).append("\n");
-            }
+            HtmlUtil.suiteInclude(buffer, "TearDown", wikiPage, SuiteResponder.SUITE_TEARDOWN_NAME);
+
             if (includeSuiteSetup) {
-                WikiPage suiteTeardown = PageCrawlerImpl.getInheritedPage(SuiteResponder.SUITE_TEARDOWN_NAME, wikiPage);
-                if (suiteTeardown != null) {
-                    WikiPagePath pagePath = wikiPage.getPageCrawler().getFullPath(suiteTeardown);
-                    String pagePathName = PathParser.render(pagePath);
-                    buffer.append("!include -teardown .").append(pagePathName).append("\n");
-                }
+                HtmlUtil.suiteInclude(buffer, SuiteResponder.SUITE_TEARDOWN_NAME,
+                                      wikiPage, SuiteResponder.SUITE_TEARDOWN_NAME);
             }
         }
 
@@ -47,4 +44,32 @@ public class HtmlUtil {
         return pageData.getHtml();
     }
 
+    /**
+     * Append includes in wikipage for test suite setup and teardown.
+     *
+     * @param buffer: buffer to format to
+     * @param pageName: page to fetch the suite page from
+     * @param wikiPage: wiki entry to to operate on
+     * @param setup: which variant of setup/teardown to chose
+     */
+    public static void suiteInclude(StringBuilder buffer, String pageName,
+                                    WikiPage wikiPage, String setup) throws Exception {
+        WikiPage suitePage = PageCrawlerImpl.getInheritedPage(pageName, wikiPage);
+
+        if (suitePage != null) {
+            WikiPagePath pagePath = wikiPage.getPageCrawler().getFullPath(suitePage);
+            String pagePathName = PathParser.render(pagePath);
+
+            if (setup == SuiteResponder.SUITE_SETUP_NAME) {
+                buffer.append("!include -setup .");
+            }
+            else if (setup == SuiteResponder.SUITE_TEARDOWN_NAME) {
+                buffer.append("!include -teardown .");
+            }
+            else {
+                throw new Exception("unknown suite setup variant");
+            }
+            buffer.append(pagePathName).append("\n");
+        }
+    }
 }
