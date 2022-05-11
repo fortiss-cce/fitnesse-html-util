dbe11125f1f78f34df4bb252aa59e9edcf161828
Philipp Czerner <suyjuris.gi@nicze.de>
2022-05-11 15:25:33 +0200

    Refactoring

--- Python/fitnesse/html_util.py
+++ Python/fitnesse/html_util.py
@@ -2,41 +2,64 @@ from io import StringIO
 
 from fitnesse.context import SuiteResponder, PageCrawlerImpl, PageData, PathParser, WikiPage, WikiPagePath
 
+class Stage:
+    typ: str
+    page_name: Optional[str]
+    include_options: list[str]
+
+    def __init__(self, typ: str, page_name: Optional[str] = None, include_options: list[str] = []):
+        assert self.typ in ('content', 'suite')
+        self.typ = typ
+        self.page_name = page_name
+        self.include_options = include_options.copy()
 
 class HtmlUtil:
+    STAGES = [
+        Stage('suite', 'setup', SuiteResponder.SUITE_SETUP_NAME),
+        Stage('suite', 'setup', 'SetUp'),
+        Stage('content'),
+        Stage('suite', 'teardown', 'TearDown'),
+        Stage('suite', 'teardown', SuiteResponder.SUITE_TEARDOWN_NAME),
+    ]
 
     @staticmethod
-    def testable_html(page_data: PageData, include_suite_setup: bool, page_crawler: PageCrawlerImpl, path_parser: PathParser) -> str:
+    def _write_suite(page_crawler, wiki_page, stage):
+        page: WikiPage = page_crawler.get_inherited_page(name, wiki_page)
+        if page is None: return
+        
+        page_path: WikiPagePath = wiki_page.get_page_crawler().get_full_path(stage.page_name)
+        page_path_name: str = path_parser.render(page_path)
+        options: str = ' '.join('-' + i for i in stage.include_options)
+        
+        string_io.writelines([f"!include {options} .", page_path_name, "\n"])
+            
+    @staticmethod
+    def testable_html(
+            page_data: PageData,
+            include_suite_setup: bool,
+            page_crawler: PageCrawlerImpl,
+            path_parser: PathParser
+    ) -> str:
+
+        stages = [Stage('content')]
+        if page_data.has_attribute("Test"):
+            stages.insert(0, Stage('suite', 'setup',    'SetUp'))
+            stages.append(   Stage('suite', 'teardown', 'TearDown'))
+
+            if include_suite_setup:
+                stages.insert(0, Stage('suite', 'setup',    SuiteResponder.SUITE_SETUP_NAME))
+                stages.append(   Stage('suite', 'teardown', SuiteResponder.SUITE_TEARDOWN_NAME))
+
         wiki_page: WikiPage = page_data.get_wiki_page()
         string_io: StringIO = StringIO()
-
-        if page_data.has_attribute("Test"):
-            if include_suite_setup:
-                suite_setup: WikiPage = page_crawler.get_inherited_page(SuiteResponder.SUITE_SETUP_NAME, wiki_page)
-                if suite_setup is not None:
-                    page_path: WikiPagePath = wiki_page.get_page_crawler().get_full_path(suite_setup)
-                    page_path_name: str = path_parser.render(page_path)
-                    string_io.writelines(["!include -setup .", page_path_name, "\n"])
-
-            setup: WikiPage = page_crawler.get_inherited_page("SetUp", wiki_page)
-            if setup is not None:
-                setup_path: WikiPagePath = wiki_page.get_page_crawler().get_full_path(setup)
-                setup_path_name: str = path_parser.render(setup_path)
-                string_io.writelines(["!include -setup .", setup_path_name, "\n"])
-
-        string_io.writelines([page_data.get_content()])
-        if page_data.has_attribute("Test"):
-            teardown: WikiPage = page_crawler.get_inherited_page("TearDown", wiki_page)
-            if teardown is not None:
-                tear_down_path: WikiPagePath = wiki_page.get_page_crawler().get_full_path(teardown)
-                tear_down_path_name: str = path_parser.render(tear_down_path)
-                string_io.writelines(["!include -teardown .", tear_down_path_name, "\n"])
-            if include_suite_setup:
-                suite_teardown: WikiPage = page_crawler.get_inherited_page(SuiteResponder.SUITE_TEARDOWN_NAME, wiki_page)
-                if suite_teardown is not None:
-                    page_path: WikiPagePath = wiki_page.get_page_crawler().get_full_path(suite_teardown)
-                    page_path_name: str = path_parser.render(page_path)
-                    string_io.writelines(["!include -teardown .", page_path_name, "\n"])
+                
+        for stage in stages:
+            if stage.typ == 'suite':
+                _write_suite(page_crawler, wiki_page, stage)
+            elif stage.typ == 'content':
+                string_io.writelines([page_data.get_content()])
+            else:
+                assert False
 
         page_data.set_content(string_io.getvalue())
         return page_data.get_html()
