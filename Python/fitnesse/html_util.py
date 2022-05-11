0c0af5425baedc6ae3a58f8ade5cfc331579dcc8
Nils <ge97buz@tum.de>
2022-05-11 15:30:24 +0200

    second refactor

--- Python/fitnesse/html_util.py
+++ Python/fitnesse/html_util.py
@@ -17,15 +17,15 @@ class HtmlUtil:
 
         if page_data.has_attribute("Test"):
             if include_suite_setup:
-                HtmlUtil.suite_setup(page_crawler, wiki_page, string_io, path_parser)
+                HtmlUtil._suite_setup(page_crawler, wiki_page, string_io, path_parser)
 
-            HtmlUtil.setup(page_crawler, wiki_page, string_io, path_parser)
+            HtmlUtil._setup(page_crawler, wiki_page, string_io, path_parser)
 
             string_io.writelines([page_data.get_content()])
 
-            HtmlUtil.teardown(page_crawler, wiki_page, string_io, path_parser)
+            HtmlUtil._teardown(page_crawler, wiki_page, string_io, path_parser)
             if include_suite_setup:
-                HtmlUtil.suite_teardown(page_crawler, wiki_page, string_io, path_parser)
+                HtmlUtil._suite_teardown(page_crawler, wiki_page, string_io, path_parser)
 
         else:
             string_io.writelines([page_data.get_content()])           
@@ -35,31 +35,29 @@ class HtmlUtil:
 
 
     @staticmethod
-    def suite_setup(page_crawler, wiki_page, string_io, path_parser):
-        suite_setup: WikiPage = page_crawler.get_inherited_page(SuiteResponder.SUITE_SETUP_NAME, wiki_page)
-        if suite_setup is not None:
-            HtmlUtil.write(wiki_page, string_io, path_parser, path=suite_setup, mode=Mode.SETUP)
+    def _suite_setup(page_crawler, wiki_page, string_io, path_parser):
+        HtmlUtil._write_page(page_crawler, wiki_page, string_io, path_parser, SuiteResponder.SUITE_SETUP_NAME, Mode.SETUP)
 
     @staticmethod
-    def setup(page_crawler, wiki_page, string_io, path_parser):
-        setup: WikiPage = page_crawler.get_inherited_page("SetUp", wiki_page)
-        if setup is not None:
-            HtmlUtil.write(wiki_page, string_io, path_parser, path=setup, mode=Mode.SETUP)
-            
-    @staticmethod
-    def teardown(page_crawler, wiki_page, string_io, path_parser):
-        teardown: WikiPage = page_crawler.get_inherited_page("TearDown", wiki_page)
-        if teardown is not None:
-            HtmlUtil.write(wiki_page, string_io, path_parser, path=teardown, mode=Mode.TEARDOWN)
-            
-    @staticmethod
-    def suite_teardown(page_crawler, wiki_page, string_io, path_parser):
-        suite_teardown: WikiPage = page_crawler.get_inherited_page(SuiteResponder.SUITE_TEARDOWN_NAME, wiki_page)
-        if suite_teardown is not None:
-            HtmlUtil.write(wiki_page, string_io, path_parser, path=suite_teardown, mode=Mode.TEARDOWN)
+    def _suite_teardown(page_crawler, wiki_page, string_io, path_parser):
+        HtmlUtil._write_page(page_crawler, wiki_page, string_io, path_parser, SuiteResponder.SUITE_TEARDOWN_NAME, Mode.TEARDOWN)
 
     @staticmethod
-    def write(wiki_page, string_io, path_parser, path: WikiPage, mode: Mode):
-        page_path: WikiPagePath = wiki_page.get_page_crawler().get_full_path(path)
-        page_path_name: str = path_parser.render(page_path)
-        string_io.writelines([f"!include -{mode.value} .", page_path_name, "\n"])
+    def _setup(page_crawler, wiki_page, string_io, path_parser):
+        HtmlUtil._write_page(page_crawler, wiki_page, string_io, path_parser, "SetUp", Mode.SETUP)
+            
+    @staticmethod
+    def _teardown(page_crawler, wiki_page, string_io, path_parser):
+        HtmlUtil._write_page(page_crawler, wiki_page, string_io, path_parser, "TearDown", Mode.TEARDOWN)
+        
+    @staticmethod
+    def _write_page(page_crawler, wiki_page, string_io, path_parser, page_name, mode):
+        path: WikiPage = page_crawler.get_inherited_page(page_name, wiki_page)
+        HtmlUtil._write(wiki_page, string_io, path_parser, path=path, mode=mode)
+        
+    @staticmethod
+    def _write(wiki_page, string_io, path_parser, path: WikiPage, mode: Mode):
+        if path is not None:
+            page_path: WikiPagePath = wiki_page.get_page_crawler().get_full_path(path)
+            page_path_name: str = path_parser.render(page_path)
+            string_io.writelines([f"!include -{mode.value} .", page_path_name, "\n"])
