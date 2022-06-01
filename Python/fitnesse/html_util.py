7900bcafa8273009c0b29d72c0e59f1e12ee2b2f
Andre Kohn <kohn.a@outlook.com>
2022-06-01 15:34:17 +0200

    use class member function

--- Python/fitnesse/html_util.py
+++ Python/fitnesse/html_util.py
@@ -3,28 +3,32 @@ from fitnesse.context import SuiteResponder, PageCrawlerImpl, PageData, PathPars
 
 
 class HtmlUtil:
+    _page_crawler: PageCrawlerImpl
+    _path_parser: PathParser
 
-    @staticmethod
-    def testable_html(page_data: PageData, include_suite_setup: bool, page_crawler: PageCrawlerImpl, path_parser: PathParser) -> str:
+    def __init__(self, page_crawler: PageCrawlerImpl, path_parser: PathParser):
+        self._page_crawler = page_crawler
+        self._path_parser = path_parser
+
+    def build_testable_html(self, page_data: PageData, include_suite_setup: bool) -> str:
+        string_io: StringIO = StringIO()
+        wiki_page: WikiPage = page_data.get_wiki_page()
+        wiki_page_crawler: PageCrawlerImpl = wiki_page.get_page_crawler()
+
         # Bail out early if test attribute does not exist
         if not page_data.has_attribute("Test"):
             string_io.writelines([page_data.get_content()])
             page_data.set_content(string_io.getvalue())
             return page_data.get_html()
 
-        wiki_page: WikiPage = page_data.get_wiki_page()
-        wiki_page_crawler: PageCrawlerImpl = wiki_page.get_page_crawler()
-        string_io: StringIO = StringIO()
-
         # Helper to write a page
         def write_page(command: str, page_name: str, context: WikiPage):
-            suite_setup: WikiPage = page_crawler.get_inherited_page(page_name, context)
+            suite_setup: WikiPage = self._page_crawler.get_inherited_page(page_name, context)
             if suite_setup is None:
                 return
             page_path: WikiPagePath = wiki_page_crawler.get_full_path(suite_setup)
-            page_path_name: str = path_parser.render(page_path)
+            page_path_name: str = self._path_parser.render(page_path)
             string_io.writelines(["!include ", command, " .", page_path_name, "\n"])
-
 
         # Write suite setup
         if include_suite_setup:
