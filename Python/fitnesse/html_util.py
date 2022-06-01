94cc8898f424f27bdf1ec9bbae71c860e4085557
Andre Kohn <kohn.a@outlook.com>
2022-06-01 15:28:14 +0200

    simplify closure name

--- Python/fitnesse/html_util.py
+++ Python/fitnesse/html_util.py
@@ -6,7 +6,7 @@ class HtmlUtil:
 
     @staticmethod
     def testable_html(page_data: PageData, include_suite_setup: bool, page_crawler: PageCrawlerImpl, path_parser: PathParser) -> str:
-        # Bail out early if test does not exist
+        # Bail out early if test attribute does not exist
         if not page_data.has_attribute("Test"):
             string_io.writelines([page_data.get_content()])
             page_data.set_content(string_io.getvalue())
@@ -17,7 +17,7 @@ class HtmlUtil:
         string_io: StringIO = StringIO()
 
         # Helper to write a page
-        def write_page_path_to_io(command: str, page_name: str, context: WikiPage):
+        def write_page(command: str, page_name: str, context: WikiPage):
             suite_setup: WikiPage = page_crawler.get_inherited_page(page_name, context)
             if suite_setup is None:
                 return
@@ -28,16 +28,16 @@ class HtmlUtil:
 
         # Write suite setup
         if include_suite_setup:
-            write_page_path_to_io("-setup", SuiteResponder.SUITE_SETUP_NAME, wiki_page)
+            write_page("-setup", SuiteResponder.SUITE_SETUP_NAME, wiki_page)
 
         # Write page content 
-        write_page_path_to_io("-setup", "SetUp", wiki_page)
+        write_page("-setup", "SetUp", wiki_page)
         string_io.writelines([page_data.get_content()])
-        write_page_path_to_io("-teardown", "TearDown", wiki_page)
+        write_page("-teardown", "TearDown", wiki_page)
 
         # Write suite teardown
         if include_suite_setup:
-            write_page_path_to_io("-teardown", SuiteResponder.SUITE_TEARDOWN_NAME, wiki_page)
+            write_page("-teardown", SuiteResponder.SUITE_TEARDOWN_NAME, wiki_page)
 
         # Set the page data
         page_data.set_content(string_io.getvalue())
