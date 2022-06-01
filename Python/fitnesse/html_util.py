83c53b7fefc8fccda2e26413376f2555ba318d60
Richard von Seck <seck@net.in.tum.de>
2022-06-01 15:05:08 +0200

    fitnesse/html_util: Replaced redundant code with calls
    Refactored redundant code to write_pagepath_to_IO calls

--- Python/fitnesse/html_util.py
+++ Python/fitnesse/html_util.py
@@ -3,13 +3,16 @@ from io import StringIO
 from fitnesse.context import SuiteResponder, PageCrawlerImpl, PageData
 from fitnesse.context import PathParser, WikiPage, WikiPagePath
 
-
 class HtmlUtil:
 
-    def write_pagepath_to_IO(page: WikiPage, string_io: StringIO, comment: str):
-        page_crawler: WikiPagePath = wiki_page.get_page_crawler()
-        page_path: page_crawler.get_full_path(page)
-        page_path_name: str = path_parser.render(page_path)
+    @staticmethod
+    def write_pagepath_to_IO(page: WikiPage,
+                             string_io: StringIO,
+                             pparser: PathParser,
+                             comment: str):
+        page_crawler: WikiPagePath = page.get_page_crawler()
+        page_path = page_crawler.get_full_path(page)
+        page_path_name: str = pparser.render(page_path)
         string_io.writelines([f"{comment}", page_path_name, "\n"])
 
     @staticmethod
@@ -25,30 +28,38 @@ class HtmlUtil:
             if include_suite_setup:
                 suite_setup: WikiPage = page_crawler.get_inherited_page(SuiteResponder.SUITE_SETUP_NAME, wiki_page)
                 if suite_setup is not None:
-                    page_path: WikiPagePath = wiki_page.get_page_crawler().get_full_path(suite_setup)
-                    page_path_name: str = path_parser.render(page_path)
-                    string_io.writelines(["!include -setup .", page_path_name, "\n"])
+                    HtmlUtil.write_pagepath_to_IO(suite_setup,
+                                                  string_io,
+                                                  path_parser,
+                                                  "!include -setup .")
 
             setup: WikiPage = page_crawler.get_inherited_page("SetUp", wiki_page)
             if setup is not None:
-                setup_path: WikiPagePath = wiki_page.get_page_crawler().get_full_path(setup)
-                setup_path_name: str = path_parser.render(setup_path)
-                string_io.writelines(["!include -setup .", setup_path_name, "\n"])
+                    HtmlUtil.write_pagepath_to_IO(setup,
+                                                  string_io,
+                                                  path_parser,
+                                                  "!include -setup .")
 
         string_io.writelines([page_data.get_content()])
 
         if page_data.has_attribute("Test"):
             teardown: WikiPage = page_crawler.get_inherited_page("TearDown", wiki_page)
+
             if teardown is not None:
-                tear_down_path: WikiPagePath = wiki_page.get_page_crawler().get_full_path(teardown)
-                tear_down_path_name: str = path_parser.render(tear_down_path)
-                string_io.writelines(["!include -teardown .", tear_down_path_name, "\n"])
+                HtmlUtil.write_pagepath_to_IO(teardown,
+                                              string_io,
+                                              path_parser,
+                                              "!include -teardown .")
+
             if include_suite_setup:
+
                 suite_teardown: WikiPage = page_crawler.get_inherited_page(SuiteResponder.SUITE_TEARDOWN_NAME, wiki_page)
+
                 if suite_teardown is not None:
-                    page_path: WikiPagePath = wiki_page.get_page_crawler().get_full_path(suite_teardown)
-                    page_path_name: str = path_parser.render(page_path)
-                    string_io.writelines(["!include -teardown .", page_path_name, "\n"])
+                    HtmlUtil.write_pagepath_to_IO(suite_teardown,
+                                                  string_io,
+                                                  path_parser,
+                                                  "!include -teardown .")
 
         page_data.set_content(string_io.getvalue())
         return page_data.get_html()
