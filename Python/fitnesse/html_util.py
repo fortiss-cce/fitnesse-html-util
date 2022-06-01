5ce3b233b97f0653e068901f6046206d5d51c42f
Andre Kohn <kohn.a@outlook.com>
2022-06-01 15:22:04 +0200

    simplify html util

--- Python/fitnesse/html_util.py
+++ Python/fitnesse/html_util.py
@@ -1,5 +1,4 @@
 from io import StringIO
-
 from fitnesse.context import SuiteResponder, PageCrawlerImpl, PageData, PathParser, WikiPage, WikiPagePath
 
 
@@ -7,36 +6,34 @@ class HtmlUtil:
 
     @staticmethod
     def testable_html(page_data: PageData, include_suite_setup: bool, page_crawler: PageCrawlerImpl, path_parser: PathParser) -> str:
+        # Bail out early if test does not exist
+        if not page_data.has_attribute("Test"):
+            string_io.writelines([page_data.get_content()])
+            page_data.set_content(string_io.getvalue())
+            return page_data.get_html()
+
         wiki_page: WikiPage = page_data.get_wiki_page()
+        wiki_page_crawler: PageCrawlerImpl = wiki_page.get_page_crawler()
         string_io: StringIO = StringIO()
 
-        if page_data.has_attribute("Test"):
-            if include_suite_setup:
-                suite_setup: WikiPage = page_crawler.get_inherited_page(SuiteResponder.SUITE_SETUP_NAME, wiki_page)
-                if suite_setup is not None:
-                    page_path: WikiPagePath = wiki_page.get_page_crawler().get_full_path(suite_setup)
-                    page_path_name: str = path_parser.render(page_path)
-                    string_io.writelines(["!include -setup .", page_path_name, "\n"])
+        def write_page_path_to_io(command: str, page_name: str, context: WikiPage):
+            suite_setup: WikiPage = page_crawler.get_inherited_page(page_name, context)
+            if suite_setup is None:
+                return
+            page_path: WikiPagePath = wiki_page_crawler.get_full_path(suite_setup)
+            page_path_name: str = path_parser.render(page_path)
+            string_io.writelines(["!include ", command, " .", page_path_name, "\n"])
 
-            setup: WikiPage = page_crawler.get_inherited_page("SetUp", wiki_page)
-            if setup is not None:
-                setup_path: WikiPagePath = wiki_page.get_page_crawler().get_full_path(setup)
-                setup_path_name: str = path_parser.render(setup_path)
-                string_io.writelines(["!include -setup .", setup_path_name, "\n"])
 
+        if include_suite_setup:
+            write_page_path_to_io("-setup", SuiteResponder.SUITE_SETUP_NAME, wiki_page)
+
+        write_page_path_to_io("-setup", "SetUp", wiki_page)
         string_io.writelines([page_data.get_content()])
-        if page_data.has_attribute("Test"):
-            teardown: WikiPage = page_crawler.get_inherited_page("TearDown", wiki_page)
-            if teardown is not None:
-                tear_down_path: WikiPagePath = wiki_page.get_page_crawler().get_full_path(teardown)
-                tear_down_path_name: str = path_parser.render(tear_down_path)
-                string_io.writelines(["!include -teardown .", tear_down_path_name, "\n"])
-            if include_suite_setup:
-                suite_teardown: WikiPage = page_crawler.get_inherited_page(SuiteResponder.SUITE_TEARDOWN_NAME, wiki_page)
-                if suite_teardown is not None:
-                    page_path: WikiPagePath = wiki_page.get_page_crawler().get_full_path(suite_teardown)
-                    page_path_name: str = path_parser.render(page_path)
-                    string_io.writelines(["!include -teardown .", page_path_name, "\n"])
+        write_page_path_to_io("-teardown", "TearDown", wiki_page)
+
+        if include_suite_setup:
+            write_page_path_to_io("-teardown", SuiteResponder.SUITE_TEARDOWN_NAME, wiki_page)
 
         page_data.set_content(string_io.getvalue())
         return page_data.get_html()
