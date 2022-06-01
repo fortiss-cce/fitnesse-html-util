3b59652cd8a2e1511fb4e761061c05f73289667d
WilliamLindskog <williamlindskog@protonmail.com>
2022-06-01 15:54:10 +0200

    Final commit

--- Python/tests/test_html_util.py
+++ Python/tests/test_html_util.py
@@ -9,7 +9,16 @@ def test_testable_html(wiki_page: WikiPage, path_parser: PathParser, page_crawle
     crawler.add_page(root, path_parser.parse("TearDown"), "teardown")
     page: WikiPage = crawler.add_page(root, path_parser.parse("TestPage"), "the content")
 
-    html: str = HtmlUtil.testable_html(page.get_data(), False, page_crawler, path_parser)
+    html_util_obj = HtmlUtil(
+        page_data = page.get_data(),
+        path_parser = path_parser
+    )
+
+    # html: str = HtmlUtil.testable_html(page.get_data(), False, page_crawler, path_parser)
+    html = html_util_obj.testable_html(
+        include_suite_setup = False, 
+        page_crawler = page_crawler
+    )
     assert ".SetUp" in html
     assert "setup" in html
     assert ".TearDown" in html
